b14277884612f1c5a635f39f37b7ee6decabd9b6
Karotte128 <karotte128@outlook.de>
2026-05-23 15:25:01 +0200

    mc metadata generator (WIP)

---
+++ pkgs/botcraft/mc-datagen.py
@@ -0,0 +1,68 @@
+import json
+import hashlib
+import base64
+from urllib.request import urlopen
+
+def get_client_url(mc_version):
+    try:
+        # Fetch the JSON data from the initial URL
+        url = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+        response = urlopen(url)
+
+        # Check if the request was successful
+        if response.getcode() != 200:
+            raise Exception(f"Failed to fetch version manifest. Status code: {response.getcode()}")
+
+        # Read the response content
+        data = response.read().decode('utf-8')
+
+        # Parse the JSON string
+        parsed_json = json.loads(data)
+
+        # Search through versions to find the matching version
+        matching_version = next((version for version in parsed_json["versions"] if version["id"] == mc_version), None)
+
+        # Extract the URL from the matching version
+        version_url = matching_version.get("url") if matching_version else None
+
+        # Fetch the version JSON data
+        version_response = urlopen(version_url)
+
+        # Check if the request was successful
+        if version_response.getcode() != 200:
+            raise Exception(f"Failed to fetch version data. Status code: {version_response.getcode()}")
+
+        # Read the response content
+        version_data = version_response.read().decode('utf-8')
+
+        # Parse the version JSON
+        parsed_version = json.loads(version_data)
+
+        # Extract the downloads.server.url
+        client_url = parsed_version.get("downloads", {}).get("client", {}).get("url")
+
+        return client_url
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+def generate_client_hash(client_url):
+    # Initialize sha256 hash
+    h = hashlib.sha256()
+
+    # Fetch the client.jar
+    client_response = urlopen(client_url)
+
+    # Check if the request was successful
+    if client_response.getcode() != 200:
+        raise Exception(f"Failed to fetch version data. Status code: {client_response.getcode()}")
+
+    # Generate the file hash
+    h.update(client_response.read())
+    digest = h.digest()
+
+    # Format hash for nix
+    sri = "sha256-" + base64.b64encode(digest).decode()
+
+    return sri
